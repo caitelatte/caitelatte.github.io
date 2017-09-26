b03f85a1bbafe5144a4ba5bf82989bbd0832da30
Caitlin <caitelatte@gmail.com>
2017-09-26 22:27:39 +1000

    Fix p5js.html javascript error, add kaleiding sliders

--- fiddles/p5js/kaleiding.js
+++ fiddles/p5js/kaleiding.js
@@ -5,27 +5,32 @@ var numRotations = 12;
 var squaresize = 50;
 var color_pastel = 100;
 
-// var
+var pickerpastel, pickertransparency,
+    pickerspread, pickerrotations, pickersize;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight-40);
+    createCanvas(windowWidth, windowHeight-40).parent("sketchHere");
     frameRate(100);
     background(200);
-    stroke(255, 255, 255, transparency);
     colorMode(ADD);
     noStroke();
-    colors = [
-        color(color_pastel, 255, 255, transparency),
-        color(color_pastel, color_pastel, 255, transparency),
-        color(255, color_pastel, 255, transparency),
-        color(255, color_pastel, color_pastel, transparency),
-        color(255, 255, color_pastel, transparency),
-        color(color_pastel, 255, color_pastel, transparency)
-    ]
 
+    pickerpastel = createSlider(0, 255, color_pastel);
+    pickerpastel.parent("sketchInput");
+    pickertransparency = createSlider(0, 255, transparency);
+    pickertransparency.parent("sketchInput");
+    pickerspread = createSlider(50, 250, spread);
+    pickerspread.parent("sketchInput");
+    pickerrotations = createSlider(0, 24, numRotations);
+    pickerrotations.parent("sketchInput");
+    pickersize = createSlider(50, 250, squaresize);
+    pickersize.parent("sketchInput");
+
+    updateSettings();
 }
 
 function draw() {
+    updateSettings();
     var distance = frameCount % (spread*2);
     if (distance != -1) {
         translate(0, height);
@@ -41,6 +46,23 @@ function draw() {
             translate(0, -spread*2);
         }
     }
+}
+
+function updateSettings() {
+  color_pastel = pickerpastel.value();
+  transparency = pickertransparency.value();
+  spread = pickerspread.value();
+  numRotations = pickerrotations.value();
+  squaresize = pickersize.value();
+  stroke(255, 255, 255, transparency);
+  colors = [
+      color(color_pastel, 255, 255, transparency),
+      color(color_pastel, color_pastel, 255, transparency),
+      color(255, color_pastel, 255, transparency),
+      color(255, color_pastel, color_pastel, transparency),
+      color(255, 255, color_pastel, transparency),
+      color(color_pastel, 255, color_pastel, transparency)
+  ]
 }
 
 function drawRingOfShapes(numShapes=8, x=0, y=0, squaresize=100, color="#FFF") {
